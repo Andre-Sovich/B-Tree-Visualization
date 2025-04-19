9b28346859d106e3df750a5b9603c71d97688055
maj4000 <maritzaa123r@gmail.com>
2025-04-19 01:26:58 -0600

    refactored functions fully still slight postions still to be fixed but working

--- AnimationTesting/mainwindow.cpp
+++ AnimationTesting/mainwindow.cpp
@@ -2,37 +2,92 @@
 #include "./ui_mainwindow.h"
 #include <QApplication>
 #include <QTimer>//Jmods
-
+#include <QDebug>
 
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    tileCount = 0;// intialize count
     ui->setupUi(this);
-
     QObject::connect(
         ui->createButton, &QPushButton::clicked,
         this, [this]() {
-        std::string key = "5";
-        onAddTreeTile(key);});//testing key defined -MMods
+            QString inputNum = ui->lineEdit->text();// Test with input number entered from lineEdit--MMods
+            bool validNum;
+            int number = inputNum.toInt(&validNum);
+            QString key;
+            if (validNum) {
+                key = inputNum;
+                onAddTreeTile(key);//onAddTreeTile adds the key to titlemap
+            }
+    });
 
-    QObject::connect(
-        ui->compareButton, &QPushButton::clicked,
-        this, &MainWindow::onCompareTreeNodes);
-    QObject::connect(
-        ui->leftButton, &QPushButton::clicked,
-        this, &MainWindow::onLeftTreeNode);
-    QObject::connect(
-        ui->rightButton, &QPushButton::clicked,
-        this, &MainWindow::onRightTreeNode);
-    QObject::connect(
-        ui->childButton, &QPushButton::clicked,
-        this, &MainWindow::onChildTreeNode);
+    QObject::connect(ui->compareButton, &QPushButton::clicked, this, [this]() {
+        // QString ogkey= "tile" + QString::number(this->tileCount);
+        // QString ogref= "tile" + QString::number(this->tileCount++);
+        // if(tileCount>0){
+        //     onCompareTreeNodes(ogkey,ogref);
+        // }
+        if (tileKeys.size() >= 2) {
+            QString key = tileKeys[tileKeys.size() - 2];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onCompareTreeNodes(key, ref);
+        }else{
+            QString key = tileKeys[tileKeys.size() - 1];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onCompareTreeNodes(key, ref);
 
-    this->tileMap["tile1"] = ui->tile2;
-    tileCount = 1;
-    //must initialize line timer--Jmods
+        }
+    });
+    QObject::connect(ui->leftButton, &QPushButton::clicked, this, [this]() {
+        // QString ogkey= "tile" + QString::number(this->tileCount);
+        // QString ogref= "tile" + QString::number(this->tileCount++);
+        // if(tileCount>=1){
+        //     onLeftTreeNode(ogkey, ogref);
+        // }
+        if (tileKeys.size() >= 2) {
+            QString key = tileKeys[tileKeys.size() - 2];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onLeftTreeNode(key, ref);
+        }else{
+            QString key = tileKeys[tileKeys.size() - 1];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onLeftTreeNode(key, ref);
+
+        }
+    });
+    QObject::connect(ui->rightButton, &QPushButton::clicked, this, [this]() {
+        // QString ogkey= "tile" + QString::number(this->tileCount);
+        // QString ogref= "tile" + QString::number(this->tileCount++);
+        // onRightTreeNode(ogkey, ogref);
+        if (tileKeys.size() >= 2) {
+            QString key = tileKeys[tileKeys.size() - 2];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onRightTreeNode(key, ref);
+        }else{
+            QString key = tileKeys[tileKeys.size() - 1];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onRightTreeNode(key, ref);
+
+        }
+    });
+    QObject::connect(ui->childButton, &QPushButton::clicked, this, [this]() {
+        // QString ogkey= "tile" + QString::number(this->tileCount);
+        // QString ogref= "tile" + QString::number(this->tileCount++);
+        // onChildTreeNode(ogkey, ogref);//Mmods because functions refactored
+        if (tileKeys.size() >= 2) {
+            QString key = tileKeys[tileKeys.size() - 2];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onChildTreeNode(key, ref);
+        }else{
+            QString key = tileKeys[tileKeys.size() - 1];
+            QString ref = tileKeys[tileKeys.size() - 1];
+            onChildTreeNode(key, ref);
+
+        }
+    });
     lineAnimationTimer = new QTimer(this);//Jmods
     connect(lineAnimationTimer, &QTimer::timeout, this, &MainWindow::moveLine);
 }
@@ -44,26 +99,22 @@ int startingXpos = 80;
 int startingYpos = 70;// changed to 70 for onMovement
 QPushButton *currentTile;
 //QPushButton *compareTile;//Mmods
-//QHash<QString, QPushButton*> tileMap;
+
 
 MainWindow::~MainWindow()
 {
 }
 
 
-void MainWindow::onAddTreeTile(std::string& key){
+void MainWindow::onAddTreeTile(QString& key){
 
     QPushButton* newTile = new QPushButton(ui->centralwidget);
     // create new tree node
     newTile->setGeometry(340,110, 0, 0);
-    newTile->setText(QString::fromStdString(key));//Mmod
+    newTile->setText(key);//Mmod
     newTile->setProperty("remove",false);// won't remove tile if false--set to true to remove on click
-
     connect(newTile, &QPushButton::clicked, this, &MainWindow::onRemoveTile);//Mmods
     //onHighlightTile(tile);//test if function works
-
-
-
     // animate tree node
     animation = new QPropertyAnimation(newTile, "geometry");
     animation->setDuration(250);
@@ -71,12 +122,17 @@ void MainWindow::onAddTreeTile(std::string& key){
     animation->setEndValue(QRect(340,110,50,50));
     animation->start();
     newTile->show();
+    //qDebug() << "Tile count:" << tileCount;
     tileCount++;
+    //qDebug() << "Tile count:" << tileCount;
     QString tileString =  "tile" + QString::number(this->tileCount);
     this->tileMap[tileString] = newTile;
+    this->tileKeys.append(tileString);//MMods
 }
 
-void MainWindow::onMovementTree(QPushButton *currentTile, QPushButton *compareTile, char animationType){//Mmods
+void MainWindow::onMovementTree(QString& key,QString& ref, char animationType){//Mmods
+    QPushButton* currentTile = tileMap[key];// Changed all std::string to QString to follow hashmap
+    QPushButton* compareTile = tileMap[ref];
     // find new position
     QRect endPos = compareTile->geometry();
     switch(animationType) {
@@ -106,17 +162,17 @@ void MainWindow::onMovementTree(QPushButton *currentTile, QPushButton *compareTi
     animation->start();
     currentTile->show();
     connect(animation, &QPropertyAnimation::finished, this, [=]() {
-        lineAnimation(this->tileMap["tile1"], this->tileMap["tile2"]); // Call lineAnimation only after movement
+        lineAnimation(tileMap["tile1"], tileMap["tile2"]); // Call lineAnimation only after movement
 
         if (animationType == 'r') outlineNode({compareTile, currentTile});
         if (animationType == 'l') outlineNode({currentTile, compareTile});
     });
     //highlightNode({tile});//Jmod ---
-    onHighlightTile(this->tileMap["tile2"]);//test if function works--Mmod
+    onHighlightTile(this->tileMap[ref]);//test if function works--Mmod
 }
 
-void MainWindow::onCompareTreeNodes(){
-    onMovementTree(this->tileMap["tile2"], this->tileMap["tile1"], 'c');
+void MainWindow::onCompareTreeNodes(QString& key,QString& ref){
+    onMovementTree(ref,key, 'c');
 }
 
 void MainWindow::lineAnimation(QPushButton* parent, QPushButton* child){//Jmods
@@ -152,20 +208,25 @@ void MainWindow::moveLine(){//Jmods
 }
 
 
-void MainWindow::onLeftTreeNode(){
-    onMovementTree(this->tileMap["tile2"], this->tileMap["tile1"], 'l');//Mmods
+void MainWindow::onLeftTreeNode(QString& key,QString& ref){
+    onMovementTree(key, ref, 'l');
 }
 
-void MainWindow::onRightTreeNode(){
-    onMovementTree(this->tileMap["tile2"], this->tileMap["tile1"], 'r');
+void MainWindow::onRightTreeNode(QString& key,QString& ref){
+    onMovementTree(key,ref, 'r');
 }
-void MainWindow::onRemoveWidget() {
-    QPushButton* button = qobject_cast<QPushButton*>(sender());
-    delete button;
+void MainWindow::onRemoveWidget(QString& key) {
+    QPushButton* button = tileMap[key];
+    if (button) {
+        delete button;
+        tileMap.remove(key);
+    }
+    // QPushButton* button = qobject_cast<QPushButton*>(sender());
+    // delete button;
 }
 
-void MainWindow::onChildTreeNode(){
-    onMovementTree(this->tileMap["tile2"], this->tileMap["tile1"], 'h');
+void MainWindow::onChildTreeNode(QString& key,QString& ref){
+    onMovementTree(key,ref, 'h');
 }
 void MainWindow::on_pushButton_clicked()//Jmods
 {
--- AnimationTesting/mainwindow.h
+++ AnimationTesting/mainwindow.h
@@ -32,21 +32,36 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void onAddTreeTile(std::string& key);//Mmods
-    void onCompareTreeNodes();
-    void onLeftTreeNode();
-    void onRightTreeNode();
-    void onChildTreeNode();
-    void onRemoveWidget();
+    void onAddTreeTile(QString& key);//Mmods start
+    // void onCompareTreeNodes();
+    // void onLeftTreeNode();
+    // void onRightTreeNode();
+    // void onChildTreeNode();
+    // void onRemoveWidget();
+    // void lineAnimation(QPushButton* parent, QPushButton* child);
+    // void moveLine();
+    // void highlightNode(const QList<QPushButton*>& tiles);
+    // void highlightNodeWithColor(const QList<QPushButton*>& tiles, const QString& color);
+    // void shiftNode(const QList<QPushButton*>& tiles);
+    // void outlineNode(const QList<QPushButton*>& tiles);
+    // void onMovementTree(QPushButton *currentTile, QPushButton *compareTile, char animationType);
+    // void onRemoveTile();
+    // void onHighlightTile(QPushButton* highlightTile);
+
+    void onCompareTreeNodes(QString& key,QString& ref);
+    void onLeftTreeNode(QString& key,QString& ref);
+    void onRightTreeNode(QString& key,QString& ref);
+    void onChildTreeNode(QString& key,QString& ref);
+    void onRemoveWidget(QString& key);//Mmods finish
     void lineAnimation(QPushButton* parent, QPushButton* child);//Jmods
     void moveLine();
     void highlightNode(const QList<QPushButton*>& tiles);
     void highlightNodeWithColor(const QList<QPushButton*>& tiles, const QString& color);
     void shiftNode(const QList<QPushButton*>& tiles);
     void outlineNode(const QList<QPushButton*>& tiles);
-    void onMovementTree(QPushButton *currentTile, QPushButton *compareTile, char animationType); // added on MovementTree function --Mmods
-    void onRemoveTile();//Mmods
-    void onHighlightTile(QPushButton* highlightTile);//Mmods
+    void onMovementTree(QString& key,QString& ref, char animationType); // refactor  --Mmods
+    void onRemoveTile();
+    void onHighlightTile(QPushButton* highlightTile);
 
 
 private slots:
@@ -71,6 +86,7 @@ private://Jmods
     QRect outlineRect;
     QHash<QString, QPushButton*> tileMap;
     int tileCount;
+    QList<QString> tileKeys;// keep track of keysstring name
 };
 
 #endif // MAINWINDOW_H
